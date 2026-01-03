06a7bc5bcd7ad736437bfb41f40e82989a564d05
Aaris-Kazi <aariskazi@gmail.com>
2026-01-03 20:34:29 +0530

    UPDATE:: imagehandler complete

--- constants/Literals.py
+++ constants/Literals.py
@@ -4,7 +4,7 @@ PRIMARY_CAMERA = 0
 WINDOW_SIZE = (1280, 640)
 
 
-PATH = "../test_images/*.jpg"
+PATH = "static/test_images/*.jpg"
 VIDEO_PATH = "static/lane1_1.mp4"
 CAMERA_WINDOW = 'Camera Window'
 VIDEO_WINDOW = 'Video Window'
--- mediaController/ImageHandler.py
+++ mediaController/ImageHandler.py
@@ -1,9 +1,22 @@
 from . import MediaHandler
 from utils import Logs
-
+from glob import glob
+from constants.Literals import PATH
+from cv2 import imread, imshow
+import matplotlib.pyplot as plt
 class ImageHandler(MediaHandler):
     
     def handler(self):
         logs = Logs()
         log = logs.get_Logger("ImageHandler")
         log.info("Image Handler Initialized")
+
+        for img_path in glob(PATH):
+            log.info(f"Processing Image: {img_path}")
+            try:
+                img = imread(img_path)
+                plt.imshow(img)
+                plt.show()
+
+            except Exception as e:
+                log.error(f"Error processing image {img_path}: {e}")
