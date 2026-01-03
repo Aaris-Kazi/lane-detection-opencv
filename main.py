4dc17b6f498f27918dd9366000e5a2baebc60633
Aaris-Kazi <aariskazi@gmail.com>
2026-01-03 17:21:44 +0530

    UPDATE:: Application fix

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from LaneDetectionApplication import LaneDetectionApplication
 from utils import Logs
 if __name__ == "__main__":
     logs = Logs()
     log = logs.get_Logger("Main")
-    log.info("Lane Detection Initialization")
+    banner = """
+        For the Input of Video input: 1
+        For the Input of Images input: 2
+        For the Input of Camera input: 3
+    """
+    print(banner)
+    options = int(input("Please give input: "))
+    log.info("Main Initialization")
+    LaneDetectionApplication(options)
