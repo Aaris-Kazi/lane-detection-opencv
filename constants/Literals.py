79beceeeb2f37025051f102ae6177c45b8720e3a
Aaris-Kazi <aariskazi@gmail.com>
2026-01-03 17:48:14 +0530

    UPDATE:: constants added

---
+++ constants/Literals.py
@@ -0,0 +1,9 @@
+QUIT = "q"
+WAIT_KEYS = 10
+WINDOW_SIZE = (1280, 640)
+
+
+PATH = "../test_images/*.jpg"
+VIDEO_PATH = '../lane1_1.mp4'
+CAMERA_WINDOW = 'Camera Window'
+VIDEO_WINDOW = 'Video Window'
