b449cb03f2118af0623753fb1ca6be6bc7748a80
Aaris-Kazi <aariskazi@gmail.com>
2026-01-03 20:15:15 +0530

    UPDATE:: video handler changes

--- constants/Literals.py
+++ constants/Literals.py
@@ -5,6 +5,6 @@ WINDOW_SIZE = (1280, 640)
 
 
 PATH = "../test_images/*.jpg"
-VIDEO_PATH = '../lane1_1.mp4'
+VIDEO_PATH = "static/lane1_1.mp4"
 CAMERA_WINDOW = 'Camera Window'
 VIDEO_WINDOW = 'Video Window'
--- mediaController/VideoHandler.py
+++ mediaController/VideoHandler.py
@@ -1,6 +1,8 @@
 from . import MediaHandler
-from utils import Logs
-
+from utils import CommonHandler, Logs
+from cv2 import VideoCapture
+from constants.Literals import VIDEO_WINDOW
+from constants.Literals import VIDEO_PATH
 class VideoHandler(MediaHandler):
 
     
@@ -8,3 +10,5 @@ class VideoHandler(MediaHandler):
         logs = Logs()
         log = logs.get_Logger("VideoHandler")
         log.info("Video Handler Initialized")
+        cap = VideoCapture(VIDEO_PATH)
+        CommonHandler.captureHandler(cap, VIDEO_WINDOW)
